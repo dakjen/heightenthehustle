d2cd8fa9010dee9cd3dfc95c32b930e428078bac
dakotah jennifer <dakotahjennifer@gmail.com>
2025-09-29 22:14:06 -0400

    fix: Add user schema to fix build error

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,1 +1,7 @@
-// Here is where you will define your database schema.
+import { pgTable, serial, text, varchar } from 'drizzle-orm/pg-core';
+
+export const users = pgTable('users', {
+  id: serial('id').primaryKey(),
+  email: text('email').notNull().unique(),
+  password: varchar('password', { length: 256 }).notNull(),
+});
