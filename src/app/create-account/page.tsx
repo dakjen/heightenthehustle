78880d50cdf573542fcc481d58fdc4fc4f2cce00
dakotah jennifer <dakotahjennifer@gmail.com>
2025-09-30 10:27:14 -0400

    feat: Apply new color scheme

--- src/app/create-account/page.tsx
+++ src/app/create-account/page.tsx
@@ -1,7 +1,7 @@
 export default function CreateAccountPage() {
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col justify-center items-center">
-      <h1 className="text-4xl font-bold text-gray-900">Create Account Page</h1>
+      <h1 className="text-4xl font-bold text-black">Create Account Page</h1>
     </div>
   );
 }
