a9b6aa200184fcff3aadc33ea94c04429e6dc83b
dakotah jennifer <dakotahjennifer@gmail.com>
2025-10-03 13:57:33 -0400

    Comment out CREATE TYPE for business_type to fix Vercel build error

--- drizzle/0009_old_monster_badoon.sql
+++ drizzle/0009_old_monster_badoon.sql
@@ -1,5 +1,5 @@
 -- CREATE TYPE "public"."business_tax_status" AS ENUM('S-Corporation', 'C-Corporation', 'Not Applicable');--> statement-breakpoint
-CREATE TYPE "public"."business_type" AS ENUM('Sole Proprietorship', 'Partnership', 'Limited Liability Company (LLC)', 'Corporation');--> statement-breakpoint
+-- CREATE TYPE "public"."business_type" AS ENUM('Sole Proprietorship', 'Partnership', 'Limited Liability Company (LLC)', 'Corporation');--> statement-breakpoint
 ALTER TABLE "businesses" ADD COLUMN "owner_name" text NOT NULL;--> statement-breakpoint
 ALTER TABLE "businesses" ADD COLUMN "percent_ownership" numeric NOT NULL;--> statement-breakpoint
 ALTER TABLE "businesses" ADD COLUMN "business_type" "business_type" NOT NULL;--> statement-breakpoint
