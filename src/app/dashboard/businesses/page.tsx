07c21424f89ada2dab7e5dd9e261764ac5a57618
dakotah jennifer <dakotahjennifer@gmail.com>
2025-10-03 15:54:49 -0400

    Refactor business edit page to separate server/client components and fix client directive for businesses list page

--- src/app/dashboard/businesses/page.tsx
+++ src/app/dashboard/businesses/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { useState, useEffect, useCallback } from "react";
 import { useFormState } from "react-dom";
 import { createBusinessProfile, getAllUserBusinesses } from "./actions";
