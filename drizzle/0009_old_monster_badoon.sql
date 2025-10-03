af0daa03c7ed799ea8468ab63a84472d0c642456
dakotah jennifer <dakotahjennifer@gmail.com>
2025-10-03 13:59:48 -0400

    Comment out remaining ALTER TABLE statements in 0009_old_monster_badoon.sql to fix Vercel build error

--- drizzle/0009_old_monster_badoon.sql
+++ drizzle/0009_old_monster_badoon.sql
@@ -1,9 +1,9 @@
 -- CREATE TYPE "public"."business_tax_status" AS ENUM('S-Corporation', 'C-Corporation', 'Not Applicable');--> statement-breakpoint
 -- CREATE TYPE "public"."business_type" AS ENUM('Sole Proprietorship', 'Partnership', 'Limited Liability Company (LLC)', 'Corporation');--> statement-breakpoint
-ALTER TABLE "businesses" ADD COLUMN "owner_name" text NOT NULL;--> statement-breakpoint
-ALTER TABLE "businesses" ADD COLUMN "percent_ownership" numeric NOT NULL;--> statement-breakpoint
-ALTER TABLE "businesses" ADD COLUMN "business_type" "business_type" NOT NULL;--> statement-breakpoint
-ALTER TABLE "businesses" ADD COLUMN "business_tax_status" "business_tax_status" NOT NULL;--> statement-breakpoint
-ALTER TABLE "businesses" ADD COLUMN "business_description" text;--> statement-breakpoint
-ALTER TABLE "businesses" ADD COLUMN "business_industry" text NOT NULL;--> statement-breakpoint
-ALTER TABLE "businesses" ADD COLUMN "business_materials_url" text;
+-- ALTER TABLE "businesses" ADD COLUMN "owner_name" text NOT NULL;--> statement-breakpoint
+-- ALTER TABLE "businesses" ADD COLUMN "percent_ownership" numeric NOT NULL;--> statement-breakpoint
+-- ALTER TABLE "businesses" ADD COLUMN "business_type" "business_type" NOT NULL;--> statement-breakpoint
+-- ALTER TABLE "businesses" ADD COLUMN "business_tax_status" "business_tax_status" NOT NULL;--> statement-breakpoint
+-- ALTER TABLE "businesses" ADD COLUMN "business_description" text;--> statement-breakpoint
+-- ALTER TABLE "businesses" ADD COLUMN "business_industry" text NOT NULL;--> statement-breakpoint
+-- ALTER TABLE "businesses" ADD COLUMN "business_materials_url" text;
