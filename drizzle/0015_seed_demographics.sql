044cc033f7877620d8125f173b70d8ba35e550dd
dakotah jennifer <dakotahjennifer@gmail.com>
2025-10-05 11:07:44 -0400

    feat: Seed demographics table with initial data

---
+++ drizzle/0015_seed_demographics.sql
@@ -0,0 +1,13 @@
+INSERT INTO "demographics" ("name") VALUES
+('Male'),
+('Female'),
+('Non-binary'),
+('Prefer not to say'),
+('White'),
+('Black or African American'),
+('Asian'),
+('American Indian or Alaska Native'),
+('Native Hawaiian or Other Pacific Islander'),
+('Hispanic or Latino'),
+('Two or More Races'),
+('Other');
