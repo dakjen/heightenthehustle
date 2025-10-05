d9529a73ca51a04360ee51f552e880d8feabc010
dakotah jennifer <dakotahjennifer@gmail.com>
2025-10-04 23:00:01 -0400

    fix: Move setViewModeCookie to a root server action

--- src/app/dashboard/components/AdminViewToggle.tsx
+++ src/app/dashboard/components/AdminViewToggle.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import { setViewModeCookie } from "../actions";
+import { setViewModeCookie } from "../../actions";
 import { useRouter } from "next/navigation";
 
 interface AdminViewToggleProps {
