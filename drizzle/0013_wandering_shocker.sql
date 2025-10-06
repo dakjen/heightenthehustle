e7c88cdbbdfa73d942ef511333465c656f183a27
dakotah jennifer <dakotahjennifer@gmail.com>
2025-10-06 12:47:52 -0400

    Feat: Generate migration for new material upload fields

---
+++ drizzle/0013_wandering_shocker.sql
@@ -0,0 +1,10 @@
+ALTER TABLE "businesses" ADD COLUMN "material1_url" text;--> statement-breakpoint
+ALTER TABLE "businesses" ADD COLUMN "material1_title" text;--> statement-breakpoint
+ALTER TABLE "businesses" ADD COLUMN "material2_url" text;--> statement-breakpoint
+ALTER TABLE "businesses" ADD COLUMN "material2_title" text;--> statement-breakpoint
+ALTER TABLE "businesses" ADD COLUMN "material3_url" text;--> statement-breakpoint
+ALTER TABLE "businesses" ADD COLUMN "material3_title" text;--> statement-breakpoint
+ALTER TABLE "businesses" ADD COLUMN "material4_url" text;--> statement-breakpoint
+ALTER TABLE "businesses" ADD COLUMN "material4_title" text;--> statement-breakpoint
+ALTER TABLE "businesses" ADD COLUMN "material5_url" text;--> statement-breakpoint
+ALTER TABLE "businesses" ADD COLUMN "material5_title" text;
