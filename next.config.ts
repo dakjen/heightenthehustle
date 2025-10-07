840e8b135b2064214db192e6d87432328d54b7b4
dakotah jennifer <dakotahjennifer@gmail.com>
2025-10-07 18:36:09 -0400

    refactor: remove hardcoded env vars from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  env: {
-    DATABASE_URL: "postgresql://neondb_owner:npg_N7XohdTWfF5D@ep-odd-breeze-adu973kv-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require",
-    JWT_SECRET: "your_jwt_secret_goes_here",
-  },
   experimental: {
     serverActions: {
       bodySizeLimit: '10mb',
