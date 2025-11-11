794bccfe70cae1be8ced58510d85a049320134d9
Dakotah Jennifer <dakjencreativellc@gmail.com>
2025-11-11 17:37:48 -0500

    Feat: Generated migration for adding 'Cisgender' to demographicCategoryEnum

---
+++ drizzle/0009_white_felicia_hardy.sql
@@ -0,0 +1,28 @@
+ALTER TYPE "public"."demographic_category" ADD VALUE 'Male';--> statement-breakpoint
+ALTER TYPE "public"."demographic_category" ADD VALUE 'Female';--> statement-breakpoint
+ALTER TYPE "public"."demographic_category" ADD VALUE 'Non-Binary';--> statement-breakpoint
+ALTER TYPE "public"."demographic_category" ADD VALUE 'Intersex';--> statement-breakpoint
+ALTER TYPE "public"."demographic_category" ADD VALUE 'Other';--> statement-breakpoint
+ALTER TYPE "public"."demographic_category" ADD VALUE 'White';--> statement-breakpoint
+ALTER TYPE "public"."demographic_category" ADD VALUE 'Black or African American';--> statement-breakpoint
+ALTER TYPE "public"."demographic_category" ADD VALUE 'Asian';--> statement-breakpoint
+ALTER TYPE "public"."demographic_category" ADD VALUE 'American Indian or Alaska Native';--> statement-breakpoint
+ALTER TYPE "public"."demographic_category" ADD VALUE 'Native Hawaiian or Other Pacific Islander';--> statement-breakpoint
+ALTER TYPE "public"."demographic_category" ADD VALUE 'Middle Eastern or North African';--> statement-breakpoint
+ALTER TYPE "public"."demographic_category" ADD VALUE 'Two or More Races';--> statement-breakpoint
+ALTER TYPE "public"."demographic_category" ADD VALUE 'Christianity';--> statement-breakpoint
+ALTER TYPE "public"."demographic_category" ADD VALUE 'Islam';--> statement-breakpoint
+ALTER TYPE "public"."demographic_category" ADD VALUE 'Judaism';--> statement-breakpoint
+ALTER TYPE "public"."demographic_category" ADD VALUE 'Hinduism';--> statement-breakpoint
+ALTER TYPE "public"."demographic_category" ADD VALUE 'Buddhism';--> statement-breakpoint
+ALTER TYPE "public"."demographic_category" ADD VALUE 'Sikhism';--> statement-breakpoint
+ALTER TYPE "public"."demographic_category" ADD VALUE 'Baháʼí Faith';--> statement-breakpoint
+ALTER TYPE "public"."demographic_category" ADD VALUE 'Shinto';--> statement-breakpoint
+ALTER TYPE "public"."demographic_category" ADD VALUE 'Taoism';--> statement-breakpoint
+ALTER TYPE "public"."demographic_category" ADD VALUE 'Confucianism';--> statement-breakpoint
+ALTER TYPE "public"."demographic_category" ADD VALUE 'Jainism';--> statement-breakpoint
+ALTER TYPE "public"."demographic_category" ADD VALUE 'Indigenous / Traditional Beliefs';--> statement-breakpoint
+ALTER TYPE "public"."demographic_category" ADD VALUE 'Pagan / Wicca';--> statement-breakpoint
+ALTER TYPE "public"."demographic_category" ADD VALUE 'Atheist';--> statement-breakpoint
+ALTER TYPE "public"."demographic_category" ADD VALUE 'Agnostic';--> statement-breakpoint
+ALTER TYPE "public"."demographic_category" ADD VALUE 'Spiritual but Not Religious';
