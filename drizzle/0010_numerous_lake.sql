2ce40dcc7f8b4f53512ecdabc03ca0d6f029bbfd
Dakotah Jennifer <dakjencreativellc@gmail.com>
2025-11-11 17:52:19 -0500

    Feat: Generated migration for adding 'State' to locationCategoryEnum and created SQL insert script

---
+++ drizzle/0010_numerous_lake.sql
@@ -0,0 +1,2 @@
+ALTER TYPE "public"."demographic_category" ADD VALUE 'Cisgender';--> statement-breakpoint
+ALTER TYPE "public"."location_category" ADD VALUE 'State';
---
+++ insert_demographics_locations.sql
@@ -0,0 +1,100 @@
+-- For Gender Identities
+INSERT INTO demographics (name, category) VALUES ('Male', 'Gender');
+INSERT INTO demographics (name, category) VALUES ('Female', 'Gender');
+INSERT INTO demographics (name, category) VALUES ('Non-Binary', 'Gender');
+INSERT INTO demographics (name, category) VALUES ('Intersex', 'Gender');
+INSERT INTO demographics (name, category) VALUES ('Other', 'Gender');
+INSERT INTO demographics (name, category) VALUES ('Transgender', 'Gender'); -- For the checkbox, it still needs to be a demographic entry
+INSERT INTO demographics (name, category) VALUES ('Cisgender', 'Gender');
+
+-- For Race Categories
+INSERT INTO demographics (name, category) VALUES ('White', 'Race');
+INSERT INTO demographics (name, category) VALUES ('Black or African American', 'Race');
+INSERT INTO demographics (name, category) VALUES ('Asian', 'Race');
+INSERT INTO demographics (name, category) VALUES ('American Indian or Alaska Native', 'Race');
+INSERT INTO demographics (name, category) VALUES ('Native Hawaiian or Other Pacific Islander', 'Race');
+INSERT INTO demographics (name, category) VALUES ('Middle Eastern or North African', 'Race');
+INSERT INTO demographics (name, category) VALUES ('Two or More Races', 'Race');
+
+-- For Religion Categories
+INSERT INTO demographics (name, category) VALUES ('Christianity', 'Religion');
+INSERT INTO demographics (name, category) VALUES ('Islam', 'Religion');
+INSERT INTO demographics (name, category) VALUES ('Judaism', 'Religion');
+INSERT INTO demographics (name, category) VALUES ('Hinduism', 'Religion');
+INSERT INTO demographics (name, category) VALUES ('Buddhism', 'Religion');
+INSERT INTO demographics (name, category) VALUES ('Sikhism', 'Religion');
+INSERT INTO demographics (name, category) VALUES ('Baháʼí Faith', 'Religion');
+INSERT INTO demographics (name, category) VALUES ('Shinto', 'Religion');
+INSERT INTO demographics (name, category) VALUES ('Taoism', 'Religion');
+INSERT INTO demographics (name, category) VALUES ('Confucianism', 'Religion');
+INSERT INTO demographics (name, category) VALUES ('Jainism', 'Religion');
+INSERT INTO demographics (name, category) VALUES ('Indigenous / Traditional Beliefs', 'Religion');
+INSERT INTO demographics (name, category) VALUES ('Pagan / Wicca', 'Religion');
+INSERT INTO demographics (name, category) VALUES ('Atheist', 'Religion');
+INSERT INTO demographics (name, category) VALUES ('Agnostic', 'Religion');
+INSERT INTO demographics (name, category) VALUES ('Spiritual but Not Religious', 'Religion');
+
+-- For State Locations
+INSERT INTO locations (name, category) VALUES ('Alabama', 'State');
+INSERT INTO locations (name, category) VALUES ('Alaska', 'State');
+INSERT INTO locations (name, category) VALUES ('Arizona', 'State');
+INSERT INTO locations (name, category) VALUES ('Arkansas', 'State');
+INSERT INTO locations (name, category) VALUES ('California', 'State');
+INSERT INTO locations (name, category) VALUES ('Colorado', 'State');
+INSERT INTO locations (name, category) VALUES ('Connecticut', 'State');
+INSERT INTO locations (name, category) VALUES ('Delaware', 'State');
+INSERT INTO locations (name, category) VALUES ('Florida', 'State');
+INSERT INTO locations (name, category) VALUES ('Georgia', 'State');
+INSERT INTO locations (name, category) VALUES ('Hawaii', 'State');
+INSERT INTO locations (name, category) VALUES ('Idaho', 'State');
+INSERT INTO locations (name, category) VALUES ('Illinois', 'State');
+INSERT INTO locations (name, category) VALUES ('Indiana', 'State');
+INSERT INTO locations (name, category) VALUES ('Iowa', 'State');
+INSERT INTO locations (name, category) VALUES ('Kansas', 'State');
+INSERT INTO locations (name, category) VALUES ('Kentucky', 'State');
+INSERT INTO locations (name, category) VALUES ('Louisiana', 'State');
+INSERT INTO locations (name, category) VALUES ('Maine', 'State');
+INSERT INTO locations (name, category) VALUES ('Maryland', 'State');
+INSERT INTO locations (name, category) VALUES ('Massachusetts', 'State');
+INSERT INTO locations (name, category) VALUES ('Michigan', 'State');
+INSERT INTO locations (name, category) VALUES ('Minnesota', 'State');
+INSERT INTO locations (name, category) VALUES ('Mississippi', 'State');
+INSERT INTO locations (name, category) VALUES ('Missouri', 'State');
+INSERT INTO locations (name, category) VALUES ('Montana', 'State');
+INSERT INTO locations (name, category) VALUES ('Nebraska', 'State');
+INSERT INTO locations (name, category) VALUES ('Nevada', 'State');
+INSERT INTO locations (name, category) VALUES ('New Hampshire', 'State');
+INSERT INTO locations (name, category) VALUES ('New Jersey', 'State');
+INSERT INTO locations (name, category) VALUES ('New Mexico', 'State');
+INSERT INTO locations (name, category) VALUES ('New York', 'State');
+INSERT INTO locations (name, category) VALUES ('North Carolina', 'State');
+INSERT INTO locations (name, category) VALUES ('North Dakota', 'State');
+INSERT INTO locations (name, category) VALUES ('Ohio', 'State');
+INSERT INTO locations (name, category) VALUES ('Oklahoma', 'State');
+INSERT INTO locations (name, category) VALUES ('Oregon', 'State');
+INSERT INTO locations (name, category) VALUES ('Pennsylvania', 'State');
+INSERT INTO locations (name, category) VALUES ('Rhode Island', 'State');
+INSERT INTO locations (name, category) VALUES ('South Carolina', 'State');
+INSERT INTO locations (name, category) VALUES ('South Dakota', 'State');
+INSERT INTO locations (name, category) VALUES ('Tennessee', 'State');
+INSERT INTO locations (name, category) VALUES ('Texas', 'State');
+INSERT INTO locations (name, category) VALUES ('Utah', 'State');
+INSERT INTO locations (name, category) VALUES ('Vermont', 'State');
+INSERT INTO locations (name, category) VALUES ('Virginia', 'State');
+INSERT INTO locations (name, category) VALUES ('Washington', 'State');
+INSERT INTO locations (name, category) VALUES ('West Virginia', 'State');
+INSERT INTO locations (name, category) VALUES ('Wisconsin', 'State');
+INSERT INTO locations (name, category) VALUES ('Wyoming', 'State');
+
+-- For Region Locations
+INSERT INTO locations (name, category) VALUES ('Northeast', 'Region');
+INSERT INTO locations (name, category) VALUES ('Mid-Atlantic', 'Region');
+INSERT INTO locations (name, category) VALUES ('New England', 'Region');
+INSERT INTO locations (name, category) VALUES ('Midwest', 'Region');
+INSERT INTO locations (name, category) VALUES ('DMV', 'Region');
+INSERT INTO locations (name, category) VALUES ('South', 'Region');
+INSERT INTO locations (name, category) VALUES ('Southeast', 'Region');
+INSERT INTO locations (name, category) VALUES ('Southwest', 'Region');
+INSERT INTO locations (name, category) VALUES ('West', 'Region');
+INSERT INTO locations (name, category) VALUES ('North East', 'Region');
+INSERT INTO locations (name, category) VALUES ('North West', 'Region');
